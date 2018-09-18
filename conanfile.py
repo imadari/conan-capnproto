c98a59b72f5a2744c00477adceadba3f9b7bbd09
istvan <istvan.madari@vanderbilt.edu>
2018-09-18 14:07:30 -0500

    amd64 build and test

--- conanfile.py
+++ conanfile.py
@@ -1,46 +1,36 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
 
 
 class CapnprotoConan(ConanFile):
     name = "capnproto"
     version = "0.6.1"
     license = "<Put the package license here>"
-    url = "<Package recipe repository url here, for issues about the package>"
+    url = "https://github.com/imadari/conan-capnproto"
     description = "<Description of Capnproto here>"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
-    default_options = "shared=False"
+    default_options = "shared=True"
     generators = "cmake"
 
     def source(self):
-        self.run("git clone https://github.com/memsharded/hello.git")
-        self.run("cd hello && git checkout static_shared")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("hello/CMakeLists.txt", "PROJECT(MyHello)",
-                              '''PROJECT(MyHello)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
+        self.run("git clone https://github.com/sandstorm-io/capnproto.git")
+        self.run("cd capnproto && git checkout tags/v0.6.1")
+        
 
     def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="hello")
-        cmake.build()
-
-        # Explicit way:
-        # self.run('cmake %s/hello %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
+        with tools.chdir("capnproto"):
+            with tools.chdir("c++"):
+                self.run('autoreconf -i')
+                env_build = AutoToolsBuildEnvironment(self)
+                env_build.configure(args=['--enable-shared'])
+                env_build.make()
 
     def package(self):
-        self.copy("*.h", dst="include", src="hello")
-        self.copy("*hello.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.h", dst="include/capnp", src="capnproto/c++/src/capnp/")
+        self.copy("*.h", dst="include/kj", src="capnproto/c++/src/kj/")
+        self.copy("*.so", dst="lib", src="capnproto/c++/.libs/", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
 
     def package_info(self):
-        self.cpp_info.libs = ["hello"]
+        self.cpp_info.libs = ["capnp", "kj"]
 
